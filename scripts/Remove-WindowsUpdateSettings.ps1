8c05b53af82e4f4852bbee5f4e41a46015abd979
Aaron Parker <aaron@stealthpuppy.com>
2022-08-29 23:16:57 +1000

    Create Remove-WindowsUpdateSettings.ps1

---
+++ scripts/Remove-WindowsUpdateSettings.ps1
@@ -0,0 +1,19 @@
+#region Restart if running in a 32-bit session
+if (!([System.Environment]::Is64BitProcess)) {
+    if ([System.Environment]::Is64BitOperatingSystem) {
+        $Arguments = "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$($MyInvocation.MyCommand.Definition)`""
+        $ProcessPath = $(Join-Path -Path $Env:SystemRoot -ChildPath "\Sysnative\WindowsPowerShell\v1.0\powershell.exe")
+        $params = @{
+            FilePath     = $ProcessPath
+            ArgumentList = $Arguments
+            Wait         = $True
+            WindowStyle  = "Hidden"
+        }
+        Start-Process @params
+        exit 0
+    }
+}
+#endregion
+
+# Delete the policy setting created by MDT
+REG DELETE "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /f
