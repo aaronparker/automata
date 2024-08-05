07c4f27ecd2b95fa5668437c4bd8599e12ee8913
Aaron Parker <aaron@stealthpuppy.com>
2024-08-05 18:31:42 +1000

    Update import

--- mdt/Import-MdtApplicationSet.ps1
+++ mdt/Import-MdtApplicationSet.ps1
@@ -4,20 +4,23 @@
         Download applications for import into an MDT share
 #>
 [CmdletBinding()]
-Param (
-    [Parameter(Mandatory = $False)]    
+param (
+    [Parameter(Mandatory = $false)]    
     [System.String] $DeploymentShare = "E:\Deployment\Automata",
 
-    [Parameter(Mandatory = $False)]
+    [Parameter(Mandatory = $false)]
     [System.String] $RepoPath = "C:\Projects\automata",
 
-    [Parameter(Mandatory = $False)]
-    [System.String] $Definition = $(Join-Path -Path $RepoPath -ChildPath "Applications.json"),
+    [Parameter(Mandatory = $false)]
+    [System.String] $Definition = "C:\Projects\automata\mdt\Applications.json",
 
-    [Parameter(Mandatory = $False)]
+    [Parameter(Mandatory = $false)]
+    [System.String] $Source = "C:\Projects\automata\mdt\Applications",
+
+    [Parameter(Mandatory = $false)]
     [System.String] $DownloadPath = (Get-Location).Path,
 
-    [Parameter(Mandatory = $False)]
+    [Parameter(Mandatory = $false)]
     [System.String] $Drive = "DS002"
 )
 
@@ -31,31 +34,31 @@ try {
     $VerbosePreference = $VerbosePref
 }
 catch {
-    Throw $_
+    throw $_
 }
 #endregion
 
 #region Update modules
 $Repository = "PSGallery"
-If (Get-PSRepository | Where-Object { $_.Name -eq $Repository -and $_.InstallationPolicy -ne "Trusted" }) {
+if (Get-PSRepository | Where-Object { $_.Name -eq $Repository -and $_.InstallationPolicy -ne "Trusted" }) {
     try {
         Write-Host -ForegroundColor "Cyan" -Object "Trust repository: PSGallery"
         Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.208" -Force
         Set-PSRepository -Name $Repository -InstallationPolicy "Trusted"
     }
     catch {
-        Throw $_
+        throw $_
     }
 }
 
 $Modules = "Evergreen", "VcRedist"
-ForEach ($module in $Modules) {
+foreach ($module in $Modules) {
     try {
         $installedModule = Get-Module -Name $module -ListAvailable | `
             Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | `
             Select-Object -First 1
         $publishedModule = Find-Module -Name $module -ErrorAction "SilentlyContinue"
-        If (($Null -eq $installedModule) -or ([System.Version]$publishedModule.Version -gt [System.Version]$installedModule.Version)) {
+        if (($Null -eq $installedModule) -or ([System.Version]$publishedModule.Version -gt [System.Version]$installedModule.Version)) {
             Write-Host -ForegroundColor "Cyan" -Object "Install module: $module"
             $params = @{
                 Name               = $module
@@ -68,7 +71,7 @@ ForEach ($module in $Modules) {
         }
     }
     catch {
-        Throw $_
+        throw $_
     }
 }
 #endregion
@@ -76,7 +79,7 @@ ForEach ($module in $Modules) {
 #region Mount the Deployment Share
 Restore-MdtPersistentDrive
 $psDrive = Get-MdtPersistentDrive | Where-Object { $_.Path -eq $DeploymentShare } | Select-Object -First 1
-If ($Null -eq $psDrive) {
+if ($Null -eq $psDrive) {
     try {
         Write-Host -ForegroundColor "Cyan" -Object "Connect to MDT deployment share"
         $params = @{
@@ -90,7 +93,7 @@ If ($Null -eq $psDrive) {
     }
     catch [System.Exception] {
         Write-Warning -Message "$($MyInvocation.MyCommand): Failed to create MDT drive at: [$DeploymentShare]."
-        Throw $_.Exception.Message
+        throw $_.Exception.Message
     }
 }
 #endregion
@@ -101,26 +104,26 @@ try {
     $Applications = Get-Content -Path $Definition | ConvertFrom-Json
 }
 catch {
-    Throw $_
+    throw $_
 }
 #endregion
 
 #region Walk through JSON and import applications
-ForEach ($Publisher in $Applications.Publishers | Get-Member -MemberType "NoteProperty") {
-    ForEach ($App in $Applications.Publishers.($Publisher.Name) | Get-Member -MemberType "NoteProperty") {
+foreach ($Publisher in $Applications.Publishers | Get-Member -MemberType "NoteProperty") {
+    foreach ($App in $Applications.Publishers.($Publisher.Name) | Get-Member -MemberType "NoteProperty") {
 
         $Properties = $Applications.Publishers.($Publisher.Name).($App.Name)
         Write-Host -ForegroundColor "Cyan" -Object "Search: $($App.Name)."
         $AppUpdate = Invoke-Expression -Command $Properties.Filter
         $ExistingApp = Get-ChildItem -Path "$($psDrive.Name):\Applications" | Where-Object { $_.ShortName -eq $Properties.ShortName -and $_.Version -eq $AppUpdate.Version }
 
-        If ([System.Version] $AppUpdate.Version -lt [System.Version] $ExistingApp.Version) {
+        if ([System.Version] $AppUpdate.Version -lt [System.Version] $ExistingApp.Version) {
             Write-Host -ForegroundColor "Cyan" -Object "Application: $($App.Name) $($AppUpdate.Version) is lower than existing: $($ExistingApp.Name) $($ExistingApp.Version)."
         }
-        ElseIf ([System.Version] $AppUpdate.Version -eq [System.Version] $ExistingApp.Version) {
+        elseif ([System.Version] $AppUpdate.Version -eq [System.Version] $ExistingApp.Version) {
             Write-Host -ForegroundColor "Cyan" -Object "Application exists: $($App.Name) $($AppUpdate.Version)."
         }
-        Else {
+        else {
             Write-Host -ForegroundColor "Cyan" -Object "Importing application: $($App.Name) $($AppUpdate.Version)."
             New-Item -Path $(Join-Path -Path $DownloadPath -ChildPath $($App.Name)) -ItemType "Directory" -ErrorAction "SilentlyContinue" > $Null
             $Download = Save-EvergreenApp -Path $(Join-Path -Path $DownloadPath -ChildPath $($App.Name)) -InputObject $AppUpdate
@@ -131,7 +134,7 @@ ForEach ($Publisher in $Applications.Publishers | Get-Member -MemberType "NotePr
                 $params = @{
                     Path                  = "$($psDrive.Name):\Applications"
                     Enable                = "True"
-                    Reboot                = $False
+                    Reboot                = $false
                     Name                  = "$($Publisher.Name) $($Properties.ShortName) $($AppUpdate.Version)"
                     ShortName             = $Properties.ShortName
                     Version               = $AppUpdate.Version
@@ -145,22 +148,21 @@ ForEach ($Publisher in $Applications.Publishers | Get-Member -MemberType "NotePr
                 Import-MDTApplication @params
             }
             catch {
-                Throw $_
+                throw $_
             }
             #endregion
 
             #region Copy additional files
-            $Source = $([System.IO.Path]::Combine($RepoPath, "automata", "Applications", $App.Name))
             $Destination = $([System.IO.Path]::Combine($DeploymentShare, "Applications", $Publisher.Name, $($Properties.ShortName -replace " "), $AppUpdate.Version))
-            If (Test-Path -Path $Source) {
-                If (Test-Path -Path $Destination) {
+            if (Test-Path -Path $Source) {
+                if (Test-Path -Path $Destination) {
                     try {
                         Write-Host -ForegroundColor "Cyan" -Object "Copy: $Source to $Destination."
                         $params = @{
                             Path        = $Source
                             Destination = $Destination
                             Filter      = "*"
-                            Container   = $False
+                            Container   = $false
                             Recurse     = $True
                             Force       = $True
                         }
@@ -170,14 +172,14 @@ ForEach ($Publisher in $Applications.Publishers | Get-Member -MemberType "NotePr
                         Write-Warning -Message "Failed to copy from: $Source."
                     }
                 }
-                Else {
+                else {
                     Write-Warning -Message "Skip copy. Folder does not exist: $Destination."
                 }
             }
             #endregion
 
             #region Run a process
-            If ($Properties.PostImport) {
+            if ($Properties.PostImport) {
                 Push-Location -Path $Destination
                 $params = @{
                     FilePath     = $Properties.PostImport.FilePath
@@ -198,14 +200,12 @@ ForEach ($Publisher in $Applications.Publishers | Get-Member -MemberType "NotePr
 }
 #endregion
 
-
 #region VcRedists; Download the VcRedists & add to the deployment share
 Write-Host -ForegroundColor "Cyan" -Object "Importing Microsoft Visual C++ Redistributables."
 $VcPath = Join-Path -Path $DownloadPath -ChildPath "VcRedists"
-New-Item -Path $VcPath -ItemType "Directory" -ErrorAction "SilentlyContinue" > $Null
-$SaveVc = Save-VcRedist -VcList (Get-VcList) -Path $VcPath
-$AppVc = Import-VcMdtApplication -VcList (Get-VcList) -Path $VcPath -MdtPath $DeploymentShare -Silent
-$BundleVc = New-VcMdtBundle -MdtPath $DeploymentShare
+New-Item -Path $VcPath -ItemType "Directory" -ErrorAction "SilentlyContinue" | Out-Null
+Get-VcList | Save-VcList -Path $VcPath | Import-VcMdtApplication -MdtPath $DeploymentShare -Silent
+New-VcMdtBundle -MdtPath $DeploymentShare
 #endregion
 
 Write-Host -ForegroundColor "Cyan" -Object "Import complete."
